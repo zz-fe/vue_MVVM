2968497ff1c103571c0818c8b1e9bad96d20f1ac
zz-fe <214464812@qq.com>
2018-01-08 10:49:03 +0800

    log

--- computed.js
+++ computed.js
@@ -1,11 +1,10 @@
 function computed() { //具有缓存功能
     let computed = this.$options.computed;  // {key:value}
+    let self = this
     Object.keys(computed).forEach(function(key){   //拿到key 值
-      console.log(key)
-      Object.defineProperty(this, key,{
+      Object.defineProperty(self, key,{
         get: typeof computed[key] === 'function' ?  computed[key] : computed[key].get,
         set(){},
       })
     })
-
 }
